c77a772458c4cc0b0e89d2b556d6c70d65d82681
Rafe <rafe@Rafes-MacBook-Pro.local>
2024-09-01 14:26:58 +0530

    Refactoring modal components

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import  './Modal.css'
-import '../../assets/global.css'
+import React from "react";
+import "./Modal.css";
+import "../../assets/global.css";
 
 interface ModalProps {
-  children: React.ReactNode;
+  topContent: JSX.Element;
+  content: JSX.Element;
+  bottomContent: JSX.Element;
 }
 
-export const Modal:React.FC<ModalProps> = (props:ModalProps) => {
+export const Modal: React.FC<ModalProps> = ({
+    topContent,content,bottomContent
+}) => {
   return (
-    <div className='modal-overlay'>
-        <div  className='modal-container  bg-color'>
-          {props.children}
+    <div className="modal">
+      <div className="modal-box bg-color">
+        <div className="modal-top">
+            {topContent}
         </div>
+        <div className="modal-content">
+            {content}
+        </div>
+        <div className="modal-bottom">
+            {bottomContent}
+        </div>
+      </div>
     </div>
-  )
-}
-
+  );
+};
--- frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
+++ frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -6,27 +6,46 @@ import { determineModalContent } from "../../utils/RegisterModalUtils";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDisptach, RootState } from "../../../../redux/Store";
 import { decrementStep } from "../../../../redux/Slices/RegisterSlice";
+import { StyledNextButton } from "../RegisterNextButton/RegisterNextButton";
+
 
 export const RegisterModal: React.FC = () => {
   //const [step, setStep] = useState<number>(3);
 
-  const state=useSelector((state:RootState)=>state.register)
-  const dispatch:AppDisptach = useDispatch()
+  const state = useSelector((state: RootState) => state.register);
+  const dispatch: AppDisptach = useDispatch();
 
   const stepCheckBuild = () => {
     //step === 1 || step === 4 || step >= 6 ? setStep(step) : setStep(step - 1);
-    dispatch(decrementStep())
+    dispatch(decrementStep());
   };
 
   return (
-    <Modal>
-      <div className="register-container">
+    <Modal
+      topContent={
         <RegisterStepCounter step={state.step} changeStep={stepCheckBuild} />
-        <div className="register-modal-content">
-        {determineModalContent(state.step)}
-          </div>
-        
-      </div>
-    </Modal>
+      }
+      content={determineModalContent(state.step)} 
+      bottomContent={<StyledNextButton
+        disabled={false}
+        active={true}
+        color={"black"}
+        onClick={()=>{}}
+      >
+        Next
+      </StyledNextButton>}
+    />
   );
+
+  // return (
+  //   <Modal>
+  //     <div className="register-container">
+  //       <RegisterStepCounter step={state.step} changeStep={stepCheckBuild} />
+  //       <div className="register-modal-content">
+  //       {determineModalContent(state.step)}
+  //         </div>
+
+  //     </div>
+  //   </Modal>
+  // );
 };
