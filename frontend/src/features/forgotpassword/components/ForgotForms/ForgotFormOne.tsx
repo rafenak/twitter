eea98e548df501d7c347c18a6caa360cf3ec03b1
Nakhuda <rafe.nakhuda@here.com>
2024-09-09 17:07:56 +0530

    Added some forgot password page one feature

--- frontend/src/features/forgotpassword/components/ForgotForms/ForgotFormOne.tsx
+++ frontend/src/features/forgotpassword/components/ForgotForms/ForgotFormOne.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./ForgotForms.css";
+import "../../../../assets/global.css";
+import { ValidatedTextInput } from "../../../../components/ValidatedInput/ValidatedTextInput";
 
-const ForgotFormOne = () => {
-  return (
-    <div>ForgotFormOne</div>
-  )
+interface ForgotFormOneProps {
+  setCredential: (name: string) => void;
+  error: boolean;
 }
 
-export default ForgotFormOne
+export const ForgotFormOne: React.FC<ForgotFormOneProps> = ({
+  setCredential,
+  error,
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCredential(e.target.value);
+  };
+  return (
+    <div className="forgot-form-container">
+      <h1 className="forgot-form-header">Find your Social Media Account</h1>
+      <p className="forgot-form-text color-gray">
+        Enter your email,phone number, or username associated with your account
+        to change your password
+      </p>
+      <ValidatedTextInput
+        valid={!error}
+        name={"forgot"}
+        label={"Email,phone number or username"}
+        changeValue={handleChange}
+      />
+      {error ? <p className="color-red forgot-error">User not found</p> : <></>}
+    </div>
+  );
+};
--- frontend/src/features/forgotpassword/components/ForgotPasswordModal/ForgotPasswordModal.tsx
+++ frontend/src/features/forgotpassword/components/ForgotPasswordModal/ForgotPasswordModal.tsx
@@ -1,13 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from '../../../../components/modal/Modal'
 import { ForgotModalTop } from '../ForgotModalTop/ForgotModalTop'
+import { ForgotFormOne } from '../ForgotForms/ForgotFormOne'
+import { validateEmail, validatePhone } from '../../../../services/Validators'
+import axios from 'axios'
 
 export const ForgotPasswordModal:React.FC<{toggleModal:()=>void}> = ({toggleModal}) => {
+    const [credential,setCredential]=useState<string>("");
+    const [error,setError]=useState<boolean>(false);
+    const [step,setStep]=useState<number>(1)
+
+    const changeCredentials =(credential:string)=>{
+        setCredential(credential)
+    }
+
+    const searchUser = async ()=>{
+        let findUserDTO = {
+            email:"",
+            phone:"",
+            username:""
+        }
+
+        if(validateEmail(credential)){
+            findUserDTO={
+                ...findUserDTO,
+                email:credential
+            }
+        }else if(validatePhone(credential)){
+            findUserDTO={
+                ...findUserDTO,
+                phone:credential
+            }
+        }else{
+            findUserDTO={
+                ...findUserDTO,
+                username:credential
+            }
+        }
+
+        try{
+            setError(false);
+            let res = await axios.post("http://localhost:8000/auth/find",{
+                email:findUserDTO.email,
+                phone:findUserDTO.phone,
+                username:findUserDTO.username
+            })
+            let data= await res.data
+            console.log(data);  
+            setStep(2);
+        }
+        catch(e){
+            setError(true)
+        }
+    }
+
   return (
     <div>
         <Modal topContent={<ForgotModalTop closeModal={toggleModal} />}
-        content={<>content</>}
-        bottomContent={<>bottom</>}
+        content={<ForgotFormOne setCredential={changeCredentials} error={error}/>}
+        bottomContent={step===1 ? <button onClick={searchUser}>Search for user</button>:<></>}
         />
     </div>
   )
