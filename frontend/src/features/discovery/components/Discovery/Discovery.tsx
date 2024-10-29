6a2185f38021c46bcfa62786d98fb949f744f7ff
nakhuda1 <rafe.nakhuda@here.com>
2024-10-29 17:43:19 +0530

    Adding some comments

--- frontend/src/features/discovery/components/Discovery/Discovery.tsx
+++ frontend/src/features/discovery/components/Discovery/Discovery.tsx
@@ -4,6 +4,7 @@ import { DiscoverySearchBar } from "../DiscoverySearchBar/DiscoverySearchBar";
 import { DiscoverySearchDropDown } from "../DiscoverySearchDropDown/DiscoverySearchDropDown";
 import './Discovery.css'
 
+//Wrapping the Context on the discovery page
 export const Discovery: React.FC = () => {
     return (
         <DiscoveryProvider>
