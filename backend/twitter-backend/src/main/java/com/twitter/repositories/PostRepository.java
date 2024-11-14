1aa8494c43d20e6d7f6b38f9ba02acf07f34a308
nakhuda1 <rafe.nakhuda@here.com>
2024-11-14 20:25:54 +0530

    Adding next page feed feature and fixing some issue

--- backend/twitter-backend/src/main/java/com/twitter/repositories/PostRepository.java
+++ backend/twitter-backend/src/main/java/com/twitter/repositories/PostRepository.java
@@ -17,38 +17,43 @@ import java.util.Set;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-    String FEED_QUERY = "SELECT * FROM \n" +
+    String FEED_QUERY = "SELECT * \n" +
+            "FROM (\n" +
+            "    -- Fetch posts directly authored by the specified user\n" +
+            "    SELECT p.post_id, p.audience, p.content, p.posted_date, p.is_reply, p.reply_restriction, p.reply_to,\n" +
+            "           p.scheduled, p.scheduled_date, p.author_id, p.poll_id\n" +
+            "    FROM posts p \n" +
+            "    WHERE p.author_id = :id\n" +
             "\n" +
-            "\t\t(\n" +
+            "    UNION\n" +
             "\n" +
-            "\t\tSELECT p.post_id, p.audience, p.content, p.posted_date, p.is_reply, p.reply_restriction, p.reply_to,\n" +
-            "               p.scheduled, p.scheduled_date, p.author_id, p.poll_id\n" +
-            "        FROM posts p where p.author_id = :id\n" +
-            "\t\t\t\n" +
-            "\t\tUNION\n" +
+            "    -- Fetch reposted posts from users followed by the specified user\n" +
+            "    SELECT p.post_id, p.audience, p.content, p.posted_date, p.is_reply, p.reply_restriction, p.reply_to,\n" +
+            "           p.scheduled, p.scheduled_date, p.author_id, p.poll_id\n" +
+            "    FROM posts p\n" +
+            "    INNER JOIN post_repost pr ON p.post_id = pr.post_id\n" +
+            "    WHERE pr.user_id IN (\n" +
+            "        SELECT u.user_id\n" +
+            "        FROM users u\n" +
+            "        INNER JOIN \"following\" f ON u.user_id = f.following_id\n" +
+            "        WHERE f.user_id = :id AND f.following_id != :id\n" +
+            "    )\n" +
             "\n" +
-            "        SELECT p.post_id, p.audience, p.content, p.posted_date, p.is_reply, p.reply_restriction, p.reply_to,\n" +
-            "               p.scheduled, p.scheduled_date, p.author_id, p.poll_id\n" +
-            "        FROM posts p\n" +
-            "        INNER JOIN post_repost pr ON p.post_id = pr.post_id\n" +
-            "        WHERE pr.user_id IN (SELECT u.user_id\n" +
-            "                 FROM users u\n" +
-            "                 INNER JOIN \"following\" f ON u.user_id = f.following_id\n" +
-            "                 WHERE f.user_id = :id AND f.following_id != :id\n" +
-            "        )\n" +
-            "        \n" +
-            "        UNION\n" +
+            "    UNION\n" +
             "\n" +
-            "        SELECT p.post_id, p.audience, p.content, p.posted_date, p.is_reply, p.reply_restriction, p.reply_to,\n" +
-            "               p.scheduled, p.scheduled_date, p.author_id, p.poll_id\n" +
-            "        FROM posts p\n" +
-            "        WHERE p.author_id IN (SELECT u.user_id\n" +
-            "                 FROM users u\n" +
-            "                 INNER JOIN \"following\" f ON u.user_id = f.following_id\n" +
-            "                 WHERE f.user_id = :id AND f.following_id != :id\n" +
-            "        )\n" +
-            "\n" +
-            "\t\t) AS p WHERE p.posted_date <= :session_start ORDER BY p.posted_date DESC";
+            "    -- Fetch posts from users followed by the specified user\n" +
+            "    SELECT p.post_id, p.audience, p.content, p.posted_date, p.is_reply, p.reply_restriction, p.reply_to,\n" +
+            "           p.scheduled, p.scheduled_date, p.author_id, p.poll_id\n" +
+            "    FROM posts p\n" +
+            "    WHERE p.author_id IN (\n" +
+            "        SELECT u.user_id\n" +
+            "        FROM users u\n" +
+            "        INNER JOIN \"following\" f ON u.user_id = f.following_id\n" +
+            "        WHERE f.user_id = :id AND f.following_id != :id\n" +
+            "    )\n" +
+            ") AS p \n" +
+            "WHERE p.posted_date <= :session_start \n" +
+            "ORDER BY p.posted_date DESC;\n";
 
 
 //    @Query( "SELECT p from Post p WHERE p.author IN(:authors) and p.postDate <= :sessionStart")
@@ -59,7 +64,9 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
     Optional<Set<Post>> findByAuthor(AppUser author);
 
-    @Query(nativeQuery = true, value = FEED_QUERY)
+    @Query(nativeQuery = true,
+            value = FEED_QUERY,
+            countQuery = "SELECT COUNT(1) FROM posts p WHERE p.author_id = :id")
     Page<Post> findFeedPost(@Param("id") Integer userId, @Param("session_start") LocalDateTime sessionStart, Pageable pageable);
 
 
--- frontend/src/features/post/components/Post/Post.tsx
+++ frontend/src/features/post/components/Post/Post.tsx
@@ -21,6 +21,7 @@ import { convertPostDateToString } from '../../utils/PostUtils';
 import { Reply } from '../Reply/Reply';
 import { bookmarkPost, likePost, repostPost } from '../../../../redux/Slices/PostSlice';
 import { createPostImageContainer } from '../../../feed/utils/FeedUtils';
+import { useNavigate } from 'react-router-dom';
 // import { covertPostContentToElements } from '../../../../utils/EmojiUtils';
 
 interface PostProps {
@@ -38,9 +39,10 @@ interface HoverColors {
 
 export const Post: React.FC<PostProps> = ({ feedPost }) => {
     const { post, replyTo, repost, repostUser } = feedPost;
-    const token = useSelector((state:RootState)=> state.user.token)
+    const token = useSelector((state: RootState) => state.user.token)
     const dispatch: AppDisptach = useDispatch();
     const postImageContainer = useMemo(() => createPostImageContainer(feedPost.post.images), [feedPost.post.postId]);
+    const navigate = useNavigate();
 
     const [colors, setColors] = useState<HoverColors>({
         reply: '#AAB8C2',
@@ -126,14 +128,14 @@ export const Post: React.FC<PostProps> = ({ feedPost }) => {
         return count.toString();
     };
 
-    const createRepost = () =>{
+    const createRepost = () => {
         dispatch(repostPost({
             postId: post.postId,
             token: token
         }))
     }
 
-    const createLike = () =>{
+    const createLike = () => {
         dispatch(likePost({
             postId: post.postId,
             token: token
@@ -141,7 +143,7 @@ export const Post: React.FC<PostProps> = ({ feedPost }) => {
     }
 
 
-    const createBookmark = () =>{
+    const createBookmark = () => {
         dispatch(bookmarkPost({
             postId: post.postId,
             token: token
@@ -149,93 +151,93 @@ export const Post: React.FC<PostProps> = ({ feedPost }) => {
     }
 
     return (
-        <>
-        {feedPost.post &&
-            <p className='post-repost-info'>
-                <RepostSVG height={15} width={15} color={"#AAB8C2"}/>
-                {feedPost.repostUser && feedPost.repostUser.nickname} reposted
-            </p>
-        }
         <div className='post'>
-            <div className='post-left'>
-                <img className="post-pfp" src={post.author.profilePicture ? post.author.profilePicture?.imageURL : pfp} alt={`${post.author.nickname}'s pfp`} />
-            </div>
-            <div className='post-right'>
-                <div className='post-right-top'>
-                    <div className='post-user-info'>
-                        <p className='post-nickname'>{post.author.nickname}</p>
-                        {post.author.verifiedAccount && <VerifiedIcon sx={{
-                            color: '#1DA1F2',
-                            height: '20px',
-                            width: '20px'
-                        }} />}
-                        {post.author.organization && <img className='post-oragnization'
-                            src={post.author.organization.imageURL} alt={`${post.author.username}'s organization`} />}
-                        <p className='post-username'>@{post.author.username}</p>
-                        <CircleIcon sx={{
-                            width: "3.5px",
-                            height: "3.5px",
-                            color: "#657786"
-                        }} />
-                        {post.postDate && <p className='post-posted-at'>{convertPostDateToString(post.postDate)}</p>}
-                    </div>
-                    <div className='post-more'>
-                        <MoreHorizIcon sx={{
-                            width: "20px",
-                            height: "20px",
-                        }} />
-                    </div>
+            {feedPost.post &&
+                <p className='post-repost-info' onMouseOver={()=> {/** Popup a modal with the user information on Mouse Over*/}}>
+                    <RepostSVG height={16} width={16} color={"#657786"}/>
+                    <span className='post-repost-user' onClick={()=> navigate(`/${feedPost.repostUser.username}`)}>{feedPost.repostUser && feedPost.repostUser.nickname} reposted </span>
+                </p>
+            }
+            <div className='post-body-wrapper'>
+                <div className='post-left'>
+                    <img className="post-pfp" src={post.author.profilePicture ? post.author.profilePicture?. imageURL : pfp} alt={`${post.author.nickname}'s pfp`} />
                 </div>
-                <div className='post-content'>
-                    {post.content}
-                    {/* {covertPostContentToElements(content,"post")} */}
-                </div>
-                {feedPost.post.images.length > 0 && postImageContainer}
-                {replyTo && <Reply reply={replyTo}/>}
-                <div className='post-action-bar'>
-                    <div className='post-action-bar-group'>
-                        <div className='post-action-bar-blue-wrapper' id='reply' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={toggleReply}>
-                            <ReplySVG height={20} width={20} color={colors.reply} />
+                <div className='post-right'>
+                    <div className='post-right-top'>
+                        <div className='post-user-info'>
+                            <p className='post-nickname'>{post.author.nickname}</p>
+                            {post.author.verifiedAccount && <VerifiedIcon sx={{
+                                color: '#1DA1F2',
+                                height: '20px',
+                                width: '20px'
+                            }} />}
+                            {post.author.organization && <img className='post-oragnization'
+                                src={post.author.organization.imageURL} alt={`${post.author.username}'s organization`} />}
+                            <p className='post-username'>@{post.author.username}</p>
+                            <CircleIcon sx={{
+                                width: "3.5px",
+                                height: "3.5px",
+                                color: "#657786"
+                            }} />
+                            {post.postDate && <p className='post-posted-at'>{convertPostDateToString(post.postDate)}</p>}
                         </div>
-                        <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.replies?.length || 0)}</p>
-                    </div>
-
-                    <div className='post-action-bar-group'>
-                        <div className='post-action-bar-repost-wrapper' id='repost' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={createRepost}>
-                            <RepostSVG height={20} width={20} color={colors.repost} />
+                        <div className='post-more'>
+                            <MoreHorizIcon sx={{
+                                width: "20px",
+                                height: "20px",
+                            }} />
                         </div>
-                        <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.reposts.length || 0)}</p>
                     </div>
-
-                    <div className='post-action-bar-group'>
-                        <div className='post-action-bar-like-wrapper' id='like' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={createLike}>
-                            <LikeSVG height={20} width={20} color={colors.like} />
-                        </div>
-                        <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.likes.length || 0)}</p>
+                    <div className='post-content'>
+                        {post.content}
+                        {/* {covertPostContentToElements(content,"post")} */}
                     </div>
-
-                    <div className='post-action-bar-group'>
-                        <div className='post-action-bar-blue-wrapper' id='views' onMouseOver={updateHoverColors} onMouseLeave={restColors}>
-                            <ViewSVG height={20} width={20} color={colors.views} />
-                        </div>
-                        {/* To do number of replied beside it */}
-                        <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.views.length || 0)}</p>
-                    </div>
-
-                    <div className='post-action-bar-right'>
+                    {feedPost.post.images.length > 0 && postImageContainer}
+                    {replyTo && <Reply reply={replyTo} />}
+                    <div className='post-action-bar'>
                         <div className='post-action-bar-group'>
-                            <div className='post-action-bar-blue-wrapper' id='bookmark' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={createBookmark}>
-                                <BookMarkSVG height={20} width={20} color={colors.bookmark} />
-                                <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.bookmarks.length || 0)}</p>
+                            <div className='post-action-bar-blue-wrapper' id='reply' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={toggleReply}>
+                                <ReplySVG height={20} width={20} color={colors.reply} />
                             </div>
+                            <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.replies?.length || 0)}</p>
                         </div>
-                        <div className='post-action-bar-blue-wrapper' id='share' onMouseOver={updateHoverColors} onMouseLeave={restColors}>
-                            <ShareSVG height={20} width={20} color={colors.share} />
+
+                        <div className='post-action-bar-group'>
+                            <div className='post-action-bar-repost-wrapper' id='repost' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={createRepost}>
+                                <RepostSVG height={20} width={20} color={colors.repost} />
+                            </div>
+                            <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.reposts.length || 0)}</p>
+                        </div>
+
+                        <div className='post-action-bar-group'>
+                            <div className='post-action-bar-like-wrapper' id='like' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={createLike}>
+                                <LikeSVG height={20} width={20} color={colors.like} />
+                            </div>
+                            <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.likes.length || 0)}</p>
+                        </div>
+
+                        <div className='post-action-bar-group'>
+                            <div className='post-action-bar-blue-wrapper' id='views' onMouseOver={updateHoverColors} onMouseLeave={restColors}>
+                                <ViewSVG height={20} width={20} color={colors.views} />
+                            </div>
+                            {/* To do number of replied beside it */}
+                            <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.views.length || 0)}</p>
+                        </div>
+
+                        <div className='post-action-bar-right'>
+                            <div className='post-action-bar-group'>
+                                <div className='post-action-bar-blue-wrapper' id='bookmark' onMouseOver={updateHoverColors} onMouseLeave={restColors} onClick={createBookmark}>
+                                    <BookMarkSVG height={20} width={20} color={colors.bookmark} />
+                                    <p className='post-action-bar-count' style={{ color: colors.reply }}>{convertCount(post.bookmarks.length || 0)}</p>
+                                </div>
+                            </div>
+                            <div className='post-action-bar-blue-wrapper' id='share' onMouseOver={updateHoverColors} onMouseLeave={restColors}>
+                                <ShareSVG height={20} width={20} color={colors.share} />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        </>
     )
 }
